6396edb5722c9740faccfcad0ef7899e1288c65a
Sridhar725S <149698567+Sridhar725S@users.noreply.github.com>
2024-12-14 18:07:46 +0530

    Update Dashboard.js

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -11,7 +11,7 @@ const Dashboard = () => {
 
   // Define API base URL based on environment
   const API_BASE_URL = process.env.NODE_ENV === 'production' 
-    ? 'https://leave-management-system-5wch.onrender.com' 
+    ? 'https://leave-management-system-o8km.onrender.com' 
     : 'http://localhost:10000';
 
   useEffect(() => {
